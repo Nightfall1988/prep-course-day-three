cc35c82801f855ff8d930754c0e12c8b64aa3c15
Nightfall1988 <grigorijs.mamilovs@gmail.com>
2021-01-24 17:06:53 +0000

    snake does not move to opposite direction from current

--- 01-snake/src/engine/Snake.ts
+++ 01-snake/src/engine/Snake.ts
@@ -7,40 +7,58 @@ head: Cell = new Cell(2,0)
 tail: Cell[] = [new Cell(0, 0), new Cell(1, 0)]
 direction: Direction = 'Right';
 cellToGrow = 0;
+bannedDir: Direction = 'Left';
+privDir: Direction = 'Right';
 
   setDirection(direction: Direction) {
+      this.privDir = this.direction;
       this.direction = direction;
   }
 
   move() {
 
+    console.log(this.privDir)
     if (this.cellToGrow > 0) {
         this.cellToGrow--;
     } else {
         this.tail.shift();
     }
       this.tail.push(this.head);
-      switch (this.direction) {
+
+
+
+    if (this.direction != this.bannedDir) {
+        switch (this.direction) {
         case 'Right':{
             this.head = new Cell(this.head.x + 1, this.head.y)
+            this.bannedDir = 'Left';
             break
         }
-        case 'Down':{
-            this.head = new Cell(this.head.x, this.head.y + 1)
+        case 'Up': {
+            this.head = new Cell(this.head.x, this.head.y - 1)
+            this.bannedDir = 'Down';
             break
         }
 
         case 'Left':{
+            {
             this.head = new Cell(this.head.x - 1, this.head.y)
+            this.bannedDir = 'Right';
             break
-        }
+                }
+            }
 
-        case 'Up':{
-            this.head = new Cell(this.head.x, this.head.y - 1)
+        case 'Down':{
+            this.head = new Cell(this.head.x, this.head.y + 1)
+            this.bannedDir = 'Up'
             break
         }
-      }
-  }
+    } 
+    } else {
+        this.direction = this.privDir
+    }
+
+}
 
   grow() {
       this.cellToGrow += 3;
